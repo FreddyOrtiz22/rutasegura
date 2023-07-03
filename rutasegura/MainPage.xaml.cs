938a82168cfb0e6e0556140b59997cac0aa56ce9
Freddy Ortiz <e1715078984@uisrael.edu.ec>
2023-07-03 15:31:27 -0500

    Agregar archivos de proyecto.

---
+++ rutasegura/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rutasegura
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.100.116/rutasegura/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<perfilusuario> post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ObtenerDatos();
+        }
+
+        private async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<perfilusuario> listaPost = JsonConvert.DeserializeObject<List<perfilusuario>>(contenido);
+            post = new ObservableCollection<perfilusuario>(listaPost);
+            listaperfilusuario.ItemsSource = post;
+        }
+
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<perfilusuario> listaPost = JsonConvert.DeserializeObject<List<perfilusuario>>(contenido);
+            post = new ObservableCollection<perfilusuario>(listaPost);
+            listaperfilusuario.ItemsSource = post;
+        }
+    }
+}
---
+++ rutasegura/perfilusuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rutasegura
+{
+    public class perfilusuario
+    {
+        public int IdCodigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set;}
+        public string Registro { get; set; }
+        public string NumTelf { get; set;}
+        public string Direccion { get; set;}
+      
+    }
+}
